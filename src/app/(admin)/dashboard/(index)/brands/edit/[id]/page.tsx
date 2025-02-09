5909fd1bb6b78f1a175b8b2e586a9628b193f3d0
RayhanAymanMangku <22523093@students.uii.ac.id>
2025-02-10 00:17:35 +0700

    first

--- src/app/(admin)/dashboard/(index)/brands/edit/[id]/page.tsx
+++ src/app/(admin)/dashboard/(index)/brands/edit/[id]/page.tsx
@@ -2,17 +2,11 @@ import FormBrand from "@/components/(index)/brands/form-brand";
 import { getBrandById } from "../../lib/data";
 import { redirect } from "next/navigation";
 
-interface BrandPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function BrandPage({ params }: BrandPageProps) {
+export default async function BrandPage({ params }: { params: { id: string } }) {
   const brand = await getBrandById(params.id);
 
   if (!brand) {
-    return redirect("/dashboard/brands");
+    redirect("/dashboard/brands");
   }
 
   return <FormBrand type="EDIT" data={brand} />;
